581e4b6006cca7fdcb06e546411e895eb6bb07b7
pikachu <anpanwork@gmail.com>
2018-11-15 17:39:59 +0900

    main

--- Design Model/main.py
+++ Design Model/main.py
@@ -14,17 +14,10 @@ if __name__ == '__main__':
     while True:
         Game().fn_start()
 
-<<<<<<< HEAD
-        ans = input('Are you continue (yes or no) ? ')
-        if ans != 'yes':
-            break
-    
-=======
         ans = raw_input("Are you continue the game('y' or 'n') ? ")
         if 'n' in ans:
             break;
     hasattr
->>>>>>> 0b27893d67eb3c38adb28ec19bd1fc62fbd85220
     print("Bye !, See you again.")
 
 
