15743124049a0c1fbe182e98e82269ee7ba14d8b
joodongkim <fdragons5@gmail.com>
2018-11-15 09:18:33 +0900

    20181115 - Deck class modify

--- Design Model/Deck.py
+++ Design Model/Deck.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+import random
 from Card import Card
 
 class Deck:
     def __init__(self):
-        self.card = Card("2", "S")
+        self.__init_deck__()
+
+    def __init_deck__(self):
+        self.deck = range(2,15) * 4
+        self.fn_shuffle()
 
     def fn_shuffle(self, ):
-        pass
+        random.shuffle(self.deck)
 
-    def fn_deal(self, ):
-        print('한장을 주었습니다.')
-        return self.card
+    def fn_deal(self, n):
+        self.cards = []
+        if len(self.deck) < n:
+            self.__init_deck__()
+            self.fn_shuffle()
 
+        print('카드 {}장을 나누어 주었습니다.'.format(n))
+        for i in range(n):
+            card = self.deck.pop()
+            if card == 11: card = "J"
+            if card == 12: card = "Q"
+            if card == 13: card = "K"
+            if card == 14: card = "A"
+            self.cards.append(card)
+            
+        return self.cards
